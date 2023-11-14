376ea8272d9ee22e7611e9f172177e25b0f90e1f
bahador <noor.bahadory786@gmail.com>
2023-11-14 22:11:39 +0000

    completed alramClock exercise

--- week-3/alarmclock/alarmclock.js
+++ week-3/alarmclock/alarmclock.js
@@ -1,8 +1,23 @@
-function setAlarm() {}
+//When you click the `Set Alarm` button the counter at the top of the screen should change to the number you entered in the `input` field.
+// For example, if the`input` field says `10` then the title should say`Time Remaining: 00:10`.
+function setAlarm() {
+  let timeInput = document.getElementById("alarmSet").value;
+  document.getElementById("timeRemaining").innerText =
+    "Time Remaining: " + formatTime(timeInput);
+  setTimeout(playAlarm, timeInput * 1000);
+}
 
+function formatTime(seconds) {
+  let minutes = Math.floor(seconds / 60);
+  let remainingSeconds = seconds % 60;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  remainingSeconds =
+    remainingSeconds < 10 ? "0" + remainingSeconds : remainingSeconds;
+  return minutes + ":" + remainingSeconds;
+}
 // DO NOT EDIT BELOW HERE
 
-var audio = new Audio("alarmsound.mp3");
+var audio = new Audio("alarmSound.mp3");
 
 function setup() {
   document.getElementById("set").addEventListener("click", () => {
